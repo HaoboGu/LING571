71ad373ab0985d98bfb0da078f758622a4fdd1eb
HaoboGu <haobogu@outlook.com>
2018-01-22 23:27:29 -0800

    Patas test version implemented using nltk.Tree

--- hw3/hw3_parser.py
+++ hw3/hw3_parser.py
@@ -1,18 +1,9 @@
 import nltk
 import sys
-from nltk.grammar import is_nonterminal
-from nltk.grammar import is_terminal
-from nltk.grammar import Nonterminal
-from nltk.grammar import Production
 import time
 from nltk import word_tokenize
 import numpy as np
-
-
-class Tree:
-    def __init__(self):
-
-
+from nltk.tree import Tree
 
 
 def create_map(productions):
@@ -33,97 +24,98 @@ def create_map(productions):
 
 def initialize_table(n_dimension):
     """
-    Initialize a n_dimension*n_dimension table with empty sets and return it
+    Initialize a n_dimension*n_dimension table using empty lists and return it
     """
     table = np.zeros((n_dimension, n_dimension), dtype=object)
     for i in range(n_dimension):
         for j in range(n_dimension):
-            table[i][j] = set()
+            table[i][j] = []
     return table
 
 
-def find_lhs(table, i, k, j, symbol_map):
+def update_cell(table, i, k, j, symbol_map):
     """
-    Look through the grammar and find lhs X where X satisfies {X->BC in grammar && B in table[i,k] && C in table[k,j]}
-    :return:
+    For current cell [i,j], update tree list based on cell [i,k] and [k,j]
+    :return: updated table
     """
-    added_lhs = set()
     for s1 in table[i, k]:
-        for s2 in table[k, j]:
-            rhs = (s1, s2)
-            if rhs in symbol_map:
+        for s2 in table[k, j]:  # s1 and s2 are trees
+            rhs = (s1.label(), s2.label())
+            if rhs in symbol_map:  # check if rhs in current grammar's rules
                 lhs = symbol_map[rhs]
-                added_lhs = added_lhs.union(lhs)
-                # print(rhs, lhs)
-
-    return added_lhs
+                for l_symbol in lhs:
+                    table[i, j].append(Tree(l_symbol, [s1, s2]))  # add current tree to cell [i,j]
+    return table
 
 
 def cky(words, grammar):
     """
-    CKY algorithm, return a table of parsed result
-    :param words: list of words
-    :param grammar: nltk grammar
-    :return: (n+1)*(n+1) table
+        CKY algorithm, return a table of parsed result
+        :param words: list of words
+        :param grammar: nltk grammar
+        :return: (n+1)*(n+1) table
     """
     rules = grammar.productions()
     symbol_map = create_map(rules)  # map RHS to a set of LHS according to the grammar
     d = len(words)
-
     # Create a d+1*d+1 table
     # Each cell [i,j] in the table represents set of non-terminals with derivation spanning i and j
-    table = initialize_table(d+1)
-    back_dict = {}
+    table = initialize_table(d + 1)
     for j in range(1, len(words)+1):
         j_tuple = tuple([words[j-1]])
-
         if j_tuple in symbol_map:
-            table[j-1][j] = symbol_map[j_tuple]
-
-        else:
-            print(j_tuple)
-            print("This word cannot be reached by the grammar")
-        for i in range(j-2, -1, -1):  # from j-2 to 0
-            for k in range(i+1, j):  # from i+1 to j-1
-                X = find_lhs(table, i, k, j, symbol_map)
-                # if not X:
-                #     back_dict[tuple(X)] = [(i, k), (k, j)]
-
-                # print(X)
-                table[i, j] = table[i, j].union(X)
+            for symbol in symbol_map[j_tuple]:  # for all terminals
+                table[j - 1][j].append(Tree(symbol, [words[j-1]]))
+        for i in range(j - 2, -1, -1):  # from j-2 to 0
+            for k in range(i + 1, j):  # from i+1 to j-1
+                table = update_cell(table, i, k, j, symbol_map)
     return table
-
 
 
 if __name__ == "__main__":
     s = time.time()
-    use_local_file = True
+    use_local_file = False
     if use_local_file:
         # grammar_filename = "other_grammars/commandtalk_original.cfg"
         import os
         if 'hw3' in os.listdir():
             os.chdir('hw3')
-        grammar_filename = 'toy.cfg'
-        test_sentence_filename = 'toy_sentences.txt'
+        grammar_filename = 'grammar_cnf.cfg'
+        test_sentence_filename = 'sentences.txt'
         output_filename = "output_parses.txt"
-
     else:
         grammar_filename = sys.argv[1]
         test_sentence_filename = sys.argv[2]
         output_filename = sys.argv[3]
 
     gr = nltk.data.load(grammar_filename)  # load data
-    sen_file = open(test_sentence_filename)
+    start_symbol = gr.start()  # get start symbol
+    sen_file = open(test_sentence_filename)  # sentence file
     line = sen_file.readline()
+    o_file = open(output_filename, 'w')  # output file
+    s_parses = 0  # sum of parses for all sentences
+    n_lines = 0  # number of line
     while line:
-        word_seq = word_tokenize(line)
-        words_table = cky(word_seq, gr)
-        print(word_seq)
-        print(words_table)
-
+        n_lines += 1
+        n_parses = 0  # number of parses for current sentence
+        word_seq = word_tokenize(line)  # word sequence
+        words_table = cky(word_seq, gr)  # cky algorithm
+        o_file.write(line)  # print
+        trees = words_table[0][words_table.shape[1]-1]
+        for item in trees:
+            if item.label() == start_symbol:  # count parses starting with start symbol
+                o_file.write(item.__str__())
+                o_file.write('\n')
+                n_parses += 1
+        s_parses += n_parses
+        o_string = 'Number of parses: ' + str(n_parses) + '\n\n'
+        o_file.write(o_string)
         line = sen_file.readline()
-
+    # o_string = 'Average parses per sentence: ' + str(s_parses/n_lines)
+    # o_file.write(o_string)
+    o_file.close()
     sen_file.close()
+
 
 
 
