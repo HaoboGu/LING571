20972d71f67024f06050d5f3e0fa743baeffc53b
HaoboGu <haobogu@outlook.com>
2018-02-07 18:04:18 -0800

    Tested version on Patas

--- hw5/hw5_parser.py
+++ hw5/hw5_parser.py
@@ -1,19 +1,18 @@
 import nltk
 import sys
 import os
-import numpy as np
 import time
 from nltk.parse import FeatureEarleyChartParser
 
 
 if __name__ == "__main__":
     s = time.time()
-    use_local_file = True
+    use_local_file = False
     if use_local_file:
         if 'hw4' in os.listdir():
             os.chdir('hw4')
-        input_pcfg_filename = 'example_grammar.fcfg'
-        sentences_filename = 'example_sentences.txt'
+        input_pcfg_filename = 'grammar.fcfg'
+        sentences_filename = 'sentences.txt'
         output_parses_filename = "hw5_output.txt"
     else:
         input_pcfg_filename = sys.argv[1]
@@ -21,14 +20,30 @@ if __name__ == "__main__":
         output_parses_filename = sys.argv[3]
 
     gr = nltk.data.load(input_pcfg_filename, format='fcfg')  # read fcfg grammar
-    parser = FeatureEarleyChartParser(gr)
+
+    parser = FeatureEarleyChartParser(gr)  # Initialize parser
+
+    output_file = open(output_parses_filename, 'w')
+
+    # Parse sentences
     with open(sentences_filename, 'r') as sent_file:
         line = sent_file.readline().strip('\n')
         while line:
             words = nltk.word_tokenize(line)
             try:
                 parsed = parser.parse(words)
+                tree = parsed.__iter__()
+                out_tree = ''
+                for tree in parsed:
+                    out_tree = tree.pformat(margin=sys.maxsize)
+                    output_file.write(out_tree+'\n')
+                    # print(out_tree)
+                    break  # only print one possible tree
+                if not out_tree:
+                    output_file.write('\n')
             except ValueError:
-                print('A')
+                # print('\n')
+                output_file.write('\n')
             line = sent_file.readline().strip('\n')
 
+    output_file.close()
